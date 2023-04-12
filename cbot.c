eb6b5c7473e0197da6d9efd97ec50f0b00fa7f25
shevy26 <127989608+shevy26@users.noreply.github.com>
2023-04-12 16:08:39 +0530

    Update cbot.c

--- cbot.c
+++ cbot.c
@@ -1,154 +1,189 @@
-#include<stdio.h>
-#include<conio.h>
-#include<graphics.h>
-#include<dos.h>
-#include<string.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<time.h>
 void main()
 {
-   char input[200],name[20];
-   int gd=DETECT,gm;
-   initgraph(&gd,&gm,"C://TURBOC3//BGI");
-   setcolor(2);
-   rectangle(270,60,305,90);
-   setcolor(2);
-   settextstyle(10,HORIZ_DIR,1);
-   outtextxy(280,50,"C BOT");  //designing heading
-   delay(1000);
-   settextstyle(0,HORIZ_DIR,1);
-   setcolor(WHITE);
-   outtextxy(1,110,"NOTE: Press 4 for exit");
-   delay(500);
-   outtextxy(1,125,"Since I'm case sensitive! Deal with me in lowercase for better response");
-   delay(1000);
-   printf("\n\n\n\n\n\n\n\n");
-   cprintf("\n");
-   printf("Hi,I'm C bot\n");
-   printf("What's your name ?\n");
-   gets(name);
-   printf("That's great name %s!\n",name);
-    while(1)
+  char input[200], name[20];
+    time_t t;   // not a primitive datatype
+    time(&t);
+  printf("\n");
+  printf("CCCCC   B---B   | OO |   TTTTT \n");
+  printf("C       B---|   | -- |     |   \n");
+  printf("CCCCC   B---B   | OO |     |   \n");
+  printf("\n");
+  printf("\nNOTE: Press 4 for exit\n");
+  printf("Since I'm case sensitive! Deal with me in lowercase for better response\n");
+  printf("\n");
+  printf("Cbot : Hi,I'm C bot\n");
+  printf("Cbot : What's your name ?\n");
+  gets(name);
+  printf("Cbot : That's great name %s!\n", name);
+  while (1)
+  {
+    gets(input);
+    if (strcmp(input, "hi") == 0)
     {
-      gets(input);
-      if(strcmp(input,"hi")==0)
-      {
-       delay(1500);
-       printf("Hi,Nice to meet you !\n");
-      }
-      else if(strcmp(input,"what is your name?")==0)
-      {
-       delay(1500);
-       printf("My name is C bot!\n");
-      }
-        else if(strcmp(input,"who create you?")==0)
-      {
-       delay(1500);
-       printf("My builder name is shevy");
-      }
-        else if(strcmp(input,"i am not good")==0)
-      {
-       delay(1500);
-       printf("I' am sorry to hear that\n");
-      }
-        else if(strcmp(input,"i am not well")==0)
-      {
-       delay(1500);
-       printf("I'm sorry to hear that \n");
-      }
-      else if(strcmp(input,"what's your name?")==0)
-      {
-       delay(1500);
-       printf("My name is C bot!\n");
-      }
-      else if(strcmp(input,"4")==0)
-      {
-       exit(0);
-      }
-      else if(strcmp(input,"hello")==0)
-      {
-       delay(1500);
-       printf("Hey! Good to see you!\n");
-      }
-      else if(strcmp(input,"how are you?")==0)
-      {
-       delay(1500);
-       printf("Hey! Thankyou so much for asking. Well I'm great and What about you?\n");
-      }
-      else if(strcmp(input,"great")==0)
-      {
-       delay(1500);
-       printf("Good to listen you\n");
-      }
-      else if(strcmp(input,"nice")==0)
-      {
-       delay(1500);
-       printf("Great\n");
-      }
-      else if(strcmp(input,"well")==0)
-      {
-       delay(1500);
-       printf("Great\n");
-      }
-      else if(strcmp(input,"i am good")==0)
-      {
-       delay(1500);
-       printf("That's Great\n");
-      }
-      else if(strcmp(input,"i am well")==0)
-      {
-       delay(1500);
-       printf("That's Great\n");
-      }
-      else if(strcmp(input,"who build you?")==0)
-      {
-       delay(1500);
-       printf("Hi! My builder name is Shevy \n");
-      }
-      else if(strcmp(input,"you are made up of which language?")==0)
-      {
-       delay(1500);
-       printf("I'm made up of C language\n");
-      }
-        else if(strcmp(input,"you are made up of what language?")==0)
-      {
-       delay(1500);
-       printf("I'm made up of C language\n");
-      }
-      else if(strcmp(input,"what is your age?")==0)
-      {
-       delay(1500);
-       printf("Infinite! Until you delete it!\n");
-      }
-      else if(strcmp(input,"what's your age?")==0)
-      {
-       delay(1500);
-       printf("Infinite! Until you delete it!\n");
-      }
-      else if(strcmp(input,name)==0)
-      {
-       delay(1500);
-       printf("That's Great %s \n",name);
-      }
-      else if(strcmp(input,"thanks")==0)
-      {
-       delay(1500);
-       printf("No Need %s\n",name);
-      }
-      else if(strcmp(input,"thankyou")==0)
-      {
-       delay(1500);
-       printf("No Need %s\n",name);
-      }
-      else if(strcmp(input,"shevy")==0)
-      {
-       delay(1500);
-       printf("Hi! Builder. How can I help you ?\n");
-      }
-      else
-      {
-       delay(1500);
-       printf("Sorry! I am unable to understand.\n");
-      }
+
+      printf("Cbot : Hi,Nice to meet you !\n");
     }
-   getch();
-   closegraph();
+    else if (strcmp(input, "what is your name?") == 0)
+    {
+
+      printf("Cbot : My name is C bot!\n");
+    }
+     else if (strcmp(input, "okay") == 0||strcmp(input, "ok") == 0)
+    {
+
+      printf("Cbot : Hmmm...\n");
+    }
+    else if (strcmp(input, "open chrome") == 0 || strcmp(input, "run chrome") == 0 || strcmp(input, "start chrome") == 0)
+    {
+      printf("Cbot : Opening Chrome...\n");
+      system("start chrome");
+      printf("Cbot : Done...\n");
+    }
+    else if (strcmp(input, "open notepad") == 0 || strcmp(input, "run notepad") == 0 || strcmp(input, "start notepad") == 0)
+    {
+      printf("Cbot : Opening notepad...\n");
+      system("start notepad");
+      printf("Cbot : Done...\n");
+    }
+    else if (strcmp(input, "open cmd") == 0 || strcmp(input, "run cmd") == 0 || strcmp(input, "start cmd") == 0)
+    {
+      printf("Cbot : Opening cmd...\n");
+      system("start cmd");
+      printf("Cbot : Done...\n");
+    }
+    else if (strcmp(input, "who create you?") == 0)
+    {
+
+      printf("Cbot : My builder name is shevy");
+    }
+    else if (strcmp(input, "i am not good") == 0)
+    {
+
+      printf("Cbot : I' am sorry to hear that\n");
+    }
+     else if (strcmp(input, "what's up?") == 0)
+    {
+
+      printf("Cbot : Hey! all good you tell?\n");
+    }
+
+    else if (strcmp(input, "i am not well") == 0)
+    {
+
+      printf("Cbot : I'm sorry to hear that \n");
+    }
+    else if (strcmp(input, "what's your name?") == 0)
+    {
+
+      printf("Cbot : My name is C bot!\n");
+    }
+    else if (strcmp(input, "4") == 0)
+    {
+      exit(0);
+    }
+    else if (strcmp(input, "hello") == 0)
+    {
+
+      printf("Cbot : Hey! Good to see you!\n");
+    }
+    else if (strcmp(input, "how are you?") == 0)
+    {
+
+      printf("Cbot : Hey! Thankyou so much for asking. Well I'm great and What about you?\n");
+    }
+    else if (strcmp(input, "great") == 0)
+    {
+
+      printf("Cbot : Good to listen you\n");
+    }
+    else if (strcmp(input, "nice") == 0)
+    {
+
+      printf("Cbot : Great\n");
+    }
+    else if (strcmp(input, "well") == 0)
+    {
+
+      printf("Cbot : Great\n");
+    }
+    else if (strcmp(input, "i am good") == 0)
+    {
+
+      printf("Cbot : That's Great\n");
+    }
+    else if (strcmp(input, "i am well") == 0)
+    {
+
+      printf("Cbot : That's Great\n");
+    }
+    else if (strcmp(input, "who build you?") == 0||strcmp(input, "who is your mother?") == 0||strcmp(input, "who is your father?") == 0)
+    {
+
+      printf("Cbot : Hi! My builder name is Shevy \n");
+    }
+    else if (strcmp(input, "you are made up of which language?") == 0)
+    {
+
+      printf("Cbot : I'm made up of C language\n");
+    }
+    else if (strcmp(input, "you are made up of what language?") == 0)
+    {
+
+      printf("Cbot : I'm made up of C language\n");
+    }
+    else if (strcmp(input, "what is your age?") == 0||strcmp(input, "how old are you?") == 0)
+    {
+      printf("Cbot : Infinite! Until you delete it!\n");
+    }
+    else if (strcmp(input, "what's your age?") == 0)
+    {
+      printf("Cbot : Infinite! Until you delete it!\n");
+    }
+    else if (strcmp(input, name) == 0)
+    {
+      printf("Cbot : That's Great %s \n", name);
+    }
+    else if (strcmp(input, "thanks") == 0)
+    {
+      printf("Cbot : No Need %s\n", name);
+    }
+    else if (strcmp(input, "thankyou") == 0)
+    {
+      printf("Cbot : No Need %s\n", name);
+    }
+    else if (strcmp(input, "shevy") == 0)
+    {
+      printf("Cbot : Hi! Builder. How can I help you ?\n");
+    }
+     else if (strcmp(input, "are you human?") == 0||strcmp(input, "are you real?") == 0)
+    {
+
+      printf("Cbot : No, I'm a bot . I exist virtually on your device.\n");
+    }
+      else if (strcmp(input, "are you a robot?") == 0||strcmp(input, "are you a machine?") == 0)
+    {
+      printf("Cbot : Ya, I'm a bot .\n");
+    }
+      else if (strcmp(input, "what day is it today?") == 0||strcmp(input, "what is the time?") == 0)
+    {
+
+      printf("Cbot : Date & Time: %s \n",ctime(&t));
+    }
+    else if (strcmp(input, "which languages can you speak?") == 0||strcmp(input, "what languages do you know?") == 0)
+    {
+
+      printf("Cbot : Well, I'm under training you can tell my builder to upgrade me .\n");
+      printf("Email: shevyarora@outlook.com\n");
+    }
+
+    else
+    {
+      printf("Cbot : Sorry! I am unable to understand.\n");
+    }
+  }
 }
